9b304d0988f8b19086e2135e14504e8f194399f7
Terrie Ann <malobaterryann@gmail.com>
2024-11-25 13:25:54 -0800

    Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+//push method
+function destructivelyAppendCat(name="Ralph"){
+    cats.push(name);
+}
+
+//unshift method
+function destructivelyPrependCat(name="Bob"){
+    cats.unshift(name);
+}
+
+//pop method
+function destructivelyRemoveLastCat(name="Garfield"){
+    cats.pop(name);
+}
+
+//shift method
+function destructivelyRemoveFirstCat(name="Milo"){
+    cats.shift(name);
+}
+
+// ... spread operator
+function appendCat(name){
+    return[...cats, name];
+}
+let newarray=appendCat("Broom");
+console.log("newarray");
+console.log("cats");
+
+function prependCat(name){
+    return[name, ...cats];
+}
+let newcat=prependCat("Arnold");
+console.log("newcat");
+console.log(cats);
+
+//slice operator
+function removeLastCat(name){
+    return cats.slice(0 ,cats.length-1);
+    
+}
+let newCatArray=removeLastCat();
+console.log(newCatArray);
+console.log(cats);
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+let newCatsArray=removeFirstCat();
+console.log(newCatsArray);
+console.log(cats);
+
+
+
+
+
+
+
